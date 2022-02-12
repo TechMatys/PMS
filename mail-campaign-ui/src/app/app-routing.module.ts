93f187e675fa0ebc50815bfe8f92898d813e15a6
Subhash Rawat <subhash@techmatys.com>
2022-02-12 14:00:57 +0530

    design changes

--- mail-campaign-ui/src/app/app-routing.module.ts
+++ mail-campaign-ui/src/app/app-routing.module.ts
@@ -4,17 +4,16 @@ import { DashboardComponent } from './admin/pages/dashboard/dashboard.component'
 import { EmailTemplateComponent } from './admin/pages/email-template/email-template.component';
 import { RecipientGroupComponent } from './admin/pages/recipient-group/recipient-group.component';
 import { UserEmailTemplateComponent } from './admin/pages/user-email-template/user-email-template.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 const routes: Routes = [
-  {
-  path:'', component: HeaderComponent
-  },
-  {
-    path:'Dashboard', component: DashboardComponent
+   {
+    path:'', component: DashboardComponent
   },
   {
     path:'Template', component: EmailTemplateComponent
+  },
+  {
+    path:'RecipientGroup', component: RecipientGroupComponent
   },
   {
     path: 'UserEmailTemplate', component: UserEmailTemplateComponent
